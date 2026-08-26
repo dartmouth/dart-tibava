f717002e20c707d3ef2d6b8a62b0950ec138e88a
Simon Stone <simon.stone@dartmouth.edu>
2026-08-26 09:27:23 -0400

    feat: allow configuration of Celery URL via env var

--- backend/src/backend/tibava/celery.py
+++ backend/src/backend/tibava/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tibava.settings")
 
-app = Celery("tibava", broker="redis://valkey:6380")
+app = Celery("tibava", broker=os.environ.get("CELERY_BROKER_URL", "redis://valkey:6380"))
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
